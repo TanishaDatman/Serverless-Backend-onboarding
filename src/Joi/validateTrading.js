dfbb8e3a80aaeb4a7c9685381b7a61a1370eadc9
TanishaDatman <tanisha@datman.je>
2025-05-03 18:36:42 +0530

    flag validation updated

--- src/Joi/validateTrading.js
+++ src/Joi/validateTrading.js
@@ -58,18 +58,28 @@ const tradingSchema = Joi.object({
       'boolean.base': 'isSameAsRegistered must be a boolean value.',
     }),
 
-  flag: Joi.number()
-    .integer()
-    .min(0)
-    .max(1)
-    .default(0)
-    .optional()
-    .messages({
-      'number.base': 'Flag must be a number.',
-      'number.min': 'Flag must be either 0 or 1.',
-      'number.max': 'Flag must be either 0 or 1.',
-    }),
+//   flag: Joi.number()
+//     .integer()
+//     .min(0)
+//     .max(1)
+//     .default(0)
+//     .optional()
+//     .messages({
+//       'number.base': 'Flag must be a number.',
+//       'number.min': 'Flag must be either 0 or 1.',
+//       'number.max': 'Flag must be either 0 or 1.',
+//     }),
 
+// });
+
+flag: Joi.number()
+  .valid(0, 1, 2, 3)
+  .default(0)
+  .optional()
+  .messages({
+    'number.base': 'Flag must be a number.',
+    'any.only': 'Flag must be one of the following values: 0, 1, 2, or 3.',
+  }),
 });
 
 module.exports = { tradingSchema };
